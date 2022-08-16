d7f9c4eee2f6303b4242bf92443269f2399af331
Allan Johnson <80973949+johnsoncwb@users.noreply.github.com>
2022-08-15 21:23:54 -0300

    first commit

---
+++ Models/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace FornecedorApi.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; } = null!;
+    }
+}
+
